25f0ed3e2c39bf5cb6dfbcc448c33e537c573bf9
sebaseter <sebaslopezz654@gmail.com>
2022-05-09 18:01:43 -0500

    prueba_3

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-console.log("hello world");
+function hola(nombre:string){
+    const hola = "holaaaa"
+console.log(hola, nombre);
 
-const init = function (value:number) {
-    
-    console.log("hola");
-    
 }
-console.log("segundo hola");
 
+function add(num1:number, num2:number){
+    const resultado = num1 + num2
 
+    return resultado 
 
+}
 
+const resultado = 1 + 2
+console.log(resultado);
